57228edb2fd3643c0ca5ca9d5a9c46c253928d17
Fabio Alexandre Da Cruz Silva Dias <fda-cruz@c1r5s13.42lisboa.com>
2026-03-09 16:57:50 +0000

    Corrected ex2

--- ex2/EliteCard.py
+++ ex2/EliteCard.py
@@ -1,59 +1,90 @@
 from ex0.Card import Card
+from ex0.CreatureCard import CreatureCard
 from ex2.Combatable import Combatable
 from ex2.Magical import Magical
 
 
 class EliteCard(Card, Combatable, Magical):
     def __init__(self, name: str, cost: int, rarity: str,
-                 type: str, damage: int, defense: int, health: int,
+                 damage: int, defense: int, health: int,
                  combat_type: str, mana: int):
         super().__init__(name, cost, rarity)
-        self.type = type
+        if damage < 0:
+            raise ValueError("Damage cannot be negative")
         self.damage = damage
+        if defense < 0:
+            raise ValueError("Defense cannot be negative")
         self.defense = defense
+        if health <= 0:
+            raise ValueError("Health needs to be positive (> 0)")
         self.health = health
+        if combat_type is None:
+            raise ValueError("combat_type cannot be None")
+        if not (combat_type == "melee" or combat_type == "ranged"
+                or combat_type == "magical"):
+            raise ValueError("effect_type needs to be 'melee', 'ranged', "
+                             "or 'magical'")
         self.combat_type = combat_type
+        if mana < 0:
+            raise ValueError("Mana cannot be negative")
         self.mana = mana
 
     def play(self, game_state: dict) -> dict:
-        info = super().play(None)
-        info.update({"effect": "Elite unit summoned to battlefield"})
-        return info
+        return {"card_played": self.name, "mana_used": self.cost,
+                "effect": "Elite unit summoned to battlefield"}
 
     def get_card_info(self) -> dict:
         info = super().get_card_info()
-        info.update({"type": self.type, "damage": self.damage,
+        info.update({"type": "Elite", "damage": self.damage,
                      "defense": self.defense, "health": self.health})
         return info
 
     def is_playable(self, available_mana: int) -> bool:
-        return super().is_playable()
+        return super().is_playable(available_mana)
 
     def attack(self, target) -> dict:
+        if not isinstance(target, (CreatureCard, "EliteCard")):
+            raise ValueError("Attack: target must be either CreatureCard"
+                             "or EliteCard")
         return {"attacker": self.name, "target": target.name,
                 "damage": self.damage, "combat_type": self.combat_type}
 
     def defend(self, incoming_damage: int) -> dict:
-        damage_taken = self.defense - incoming_damage
-        if damage_taken < 0:
-            self.health += damage_taken
-        alive = True
-        if self.health <= 0:
-            alive = False
+        if incoming_damage < 0:
+            raise ValueError("Defend: incoming_damage cannot be negative")
+        if incoming_damage >= 0:
+            damage_taken = self.defense - incoming_damage
+            if damage_taken < 0:
+                self.health += damage_taken
         return {"defender": self.name, "damage_taken": incoming_damage,
-                "damage_blocked": self.damage, "still_alive": alive}
+                "damage_blocked": self.damage, "still_alive": self.health > 0}
 
     def get_combat_stats(self) -> dict:
         return {"attack": self.damage, "defense": self.defense,
                 "health": self.health, "combat_type": self.combat_type}
 
     def cast_spell(self, spell_name: str, targets: list) -> dict:
+        if not spell_name:
+            raise ValueError("Cast Spell: spell_name cannot be None")
+        if not targets:
+            raise ValueError("Cast Spell: targets cannot be None")
+        if len(targets) == 0:
+            raise ValueError("Cast Spell: targets cannot be empty")
+        for target in targets:
+            if not isinstance(target, (CreatureCard, "EliteCard")):
+                raise ValueError("Cast Spell: targets must have either "
+                                 "CreatureCards or EliteCards")
         target_names = [target.name for target in targets]
+        for target in targets:
+            self.attack(target)
         self.mana -= 4
         return {"caster": self.name, "spell": spell_name,
                 "targets": target_names, "mana_used": 4}
 
     def channel_mana(self, amount: int) -> dict:
+        if amount <= 0:
+            raise ValueError("Channel Mana: amount needs to be "
+                             "positive (> 0)")
         self.mana += amount
         return {"channeled": amount, "total_mana": self.mana}
 
--- ex2/main.py
+++ ex2/main.py
@@ -8,26 +8,36 @@ if __name__ == "__main__":
     print("=== DataDeck Ability System ===")
     print()
     print("EliteCard capabilities:")
-    card_methods = ["play", "get_card_info", "is_playable"]
-    print(f"- Card: {card_methods}")
-    combatable_methods = ["attack", "defend", "get_combat_stats"]
-    print(f"- Combatable: {combatable_methods}")
-    magical_methods = ["cast_spell", "channel_mana", "get_magic_stats"]
-    print(f"- Magical: {magical_methods}")
-    print()
-    print("Playing Arcane Warrior (Elite Card):")
-    card = EliteCard("Arcane Warrior", 9, Rarity.LEGENDARY.value, "Elite",
-                     5, 3, 10, "melee", 8)
-    print()
-    print("Combat phase:")
-    enemy = CreatureCard("Enemy", 2, Rarity.COMMON.value, "Creature", 2, 8)
-    print(f"Attack result: {card.attack(enemy)}")
-    print(f"Defense result: {card.defend(enemy.attack)}")
-    print()
-    print("Magic phase:")
-    enemy1 = CreatureCard("Enemy1", 2, Rarity.COMMON.value, "Creature", 2, 8)
-    enemy2 = CreatureCard("Enemy2", 2, Rarity.COMMON.value, "Creature", 2, 8)
-    print(f"Spell cast: {card.cast_spell('Fireball', [enemy1, enemy2])}")
-    print(f"Mana channel: {card.channel_mana(3)}")
-    print()
-    print("Multiple interface implementation sucessful!")
+    try:
+        card_methods = ["play", "get_card_info", "is_playable"]
+        print(f"- Card: {card_methods}")
+        combatable_methods = ["attack", "defend", "get_combat_stats"]
+        print(f"- Combatable: {combatable_methods}")
+        magical_methods = ["cast_spell", "channel_mana", "get_magic_stats"]
+        print(f"- Magical: {magical_methods}")
+        print()
+        print("Playing Arcane Warrior (Elite Card):")
+        card = EliteCard("Arcane Warrior", 9, Rarity.LEGENDARY.value,
+                         5, 3, 10, "melee", 8)
+        print()
+        print("Combat phase:")
+        enemy = CreatureCard("Enemy", 2, Rarity.COMMON.value, 2, 8)
+        print(f"Attack result: {card.attack(enemy)}")
+        print(f"Defense result: {card.defend(enemy.attack)}")
+        print()
+        print("Magic phase:")
+        enemy1 = CreatureCard("Enemy1", 2, Rarity.COMMON.value, 2, 8)
+        enemy2 = CreatureCard("Enemy2", 2, Rarity.COMMON.value, 2, 8)
+        print(f"Spell cast: {card.cast_spell('Fireball', [enemy1, enemy2])}")
+        print(f"Mana channel: {card.channel_mana(3)}")
+        print()
+        print("Multiple interface implementation sucessful!")
+    except ValueError as error:
+        print(error)
+    except ImportError as error:
+        print("Somehow, ImportError appeared...")
+        print(error)
+    except TypeError as error:
+        print("TypeError: Just read the trace and correct it...")
+        print(error)
+
